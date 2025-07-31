5e419ae0d3eb8f571821c2f42979c531a22eb7a2
AlisonHSerpa <147287390+AlisonHSerpa@users.noreply.github.com>
2025-07-31 13:55:19 -0300

    correção __init__ security

--- version_5/client/src/security/__init__.py
+++ version_5/client/src/security/__init__.py
@@ -1,6 +1,6 @@
-from .keygen import Translate_Pem
+from .keygen import Keygen
 from .translate_pem import Translate_Pem
 from .diffie_helman import Diffie_Helman
 from .encrypt_rsa import EncryptionRSA
 
-__all__ = ['Translate_Pem', 'encrypt_rsa', 'Translate_Pem', 'Diffie_Helman', 'EncryptionRSA']
+__all__ = ['Keygen', 'encrypt_rsa', 'Translate_Pem', 'Diffie_Helman', 'EncryptionRSA']
