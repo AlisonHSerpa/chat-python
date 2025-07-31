d46c8d2c82b27c8fd71d176b0b23b5cd15648bda
AlisonHSerpa <147287390+AlisonHSerpa@users.noreply.github.com>
2025-07-31 13:41:33 -0300

    adicionando novas classes no __init__

--- version_5/client/src/security/__init__.py
+++ version_5/client/src/security/__init__.py
@@ -2,5 +2,6 @@ from .keygen import Translate_Pem
 from .encrypt_rsa import encrypt_rsa
 from .translate_pem import Translate_Pem
 from .diffie_helman import Diffie_Helman
+from .encrypt_rsa import EncryptionRSA
 
-__all__ = ['Translate_Pem', 'encrypt_rsa', 'Translate_Pem', 'Diffie_Helman']
+__all__ = ['Translate_Pem', 'encrypt_rsa', 'Translate_Pem', 'Diffie_Helman', 'EncryptionRSA']
